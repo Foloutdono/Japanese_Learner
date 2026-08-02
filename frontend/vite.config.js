bf543ce3da64062494dfe71be46c4ae019a6e02f
Foloutdono <folout88@gmail.com>
2026-08-02 22:37:56 +0200

    refactor: remove unused Storybook test configuration and related comments from vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,6 @@ import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -15,26 +14,4 @@ export default defineConfig({
       '/api': 'http://localhost:8000'
     }
   },
-  test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
-        }
-      }
-    }]
-  }
 });
